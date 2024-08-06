fbb6380be596194d00c2aba340a0b840158408cf
lucas <lucas.lima716@etec.sp.gov.br>
2024-08-05 21:15:44 -0300

    criação

---
+++ src/app/primeiro-projeto/primeiro-projeto.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core"
+@Component({
+selector: 'meu-primeiro-component',
+standalone: true,
+imports: [],
+template:`
+<h1>Meu Primeiro Component</h1>
+`
+
+})
+
+export class MeuPrimeiroComponent {}
---
+++ src/app/primeiro-projeto/segundo-component.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core"
+@Component({
+selector: 'meu-segundo-component',
+standalone: true,
+imports: [],
+template:`
+<h1>Meu Segundo Component</h1>
+`
+
+})
+
+export class MeuSegundoComponent {}
